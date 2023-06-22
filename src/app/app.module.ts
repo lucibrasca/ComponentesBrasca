8216499169cbcba44542ed8a75b0d8e5bc4c84fd
Maria Lucila Brasca <lucibrasca@gmail.com>
2023-06-21 23:39:03 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
